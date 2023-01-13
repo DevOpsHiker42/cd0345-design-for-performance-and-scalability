fec95a606b8e95585e42170312978d6394f65126
John Turner <john.turner@engineering.digital.dwp.gov.uk>
2023-01-13 14:47:56 +0000

    Add Task 5 / Part 1 terraform code

--- Exercise_1/main.tf
+++ Exercise_1/main.tf
@@ -1,7 +1,28 @@
-# TODO: Designate a cloud provider, region, and credentials
+# Designate a cloud provider, region, and credentials
 
+provider "aws" {
+  # access_key and secret_key values stored as environment variables for security
+  region = "us-east-1"
+}
 
-# TODO: provision 4 AWS t2.micro EC2 instances named Udacity T2
+# Provision 4 AWS t2.micro EC2 instances named Udacity T2
 
+resource "aws_instance" "Udacity_T2" {
+  ami = "ami-0b5eea76982371e91"
+  instance_type = "t2.micro"
+  count = 4
+  tags = {
+    Name = "Udacity T2"
+  }
+}
 
-# TODO: provision 2 m4.large EC2 instances named Udacity M4
+# Provision 2 m4.large EC2 instances named Udacity M4
+
+resource "aws_instance" "Udacity_M4" {
+  ami = "ami-0b5eea76982371e91"
+  instance_type = "m4.large"
+  count = 2
+  tags = {
+    Name = "Udacity T2"
+  }
+}

9389ab670ad02601269ee53a3af2ddfac1449d6c
John Turner <john.turner@engineering.digital.dwp.gov.uk>
2023-01-16 17:10:54 +0000

    add cloudwatch for lambda

--- Exercise_2/main.tf
+++ Exercise_2/main.tf
@@ -25,17 +25,55 @@ EOF
 
 resource "aws_lambda_function" "greet_lambda" {
   filename      = "lambda.py.zip"
-  function_name = "greeting_function"
+  function_name = var.lambda_function_name
   role          = aws_iam_role.iam_for_lambda.arn
-  handler       = "index.test"
+
+  depends_on = [
+    aws_iam_role_policy_attachment.lambda_logs,
+    aws_cloudwatch_log_group.greeting_lambda_logs,
+  ]
 
   source_code_hash = filebase64sha256("lambda.py.zip")
 
   runtime = "python3.9"
+  handler       = "lambda.lambda_handler"
 
   environment {
     variables = {
       greeting = "Howdy"
     }
   }
+}
+
+resource "aws_cloudwatch_log_group" "greeting_lambda_logs" {
+  name              = "/aws/lambda/${var.lambda_function_name}"
+  retention_in_days = 7
+}
+
+resource "aws_iam_policy" "lambda_logging" {
+  name        = "lambda_logging"
+  path        = "/"
+  description = "IAM policy for logging from a lambda"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ],
+      "Resource": "arn:aws:logs:*:*:*",
+      "Effect": "Allow"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_logs" {
+  role       = aws_iam_role.iam_for_lambda.name
+  policy_arn = aws_iam_policy.lambda_logging.arn
 }
--- Exercise_2/variables.tf
+++ Exercise_2/variables.tf
@@ -1,5 +1,11 @@
 variable "aws_region" {
-  description = "AWS region"
+  description = "AWS Region"
   type        = string
   default     = "us-east-1"
+}
+
+variable "lambda_function_name" {
+  description = "Lambda Function Name"
+  type        = string
+  default     = "greeting_function"
 }
